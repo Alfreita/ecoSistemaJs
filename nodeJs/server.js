2365f1a22296997074e204e49d88fb60f3a53a60
Victor Almeida <victor.freitas08@hotmail.com>
2019-02-25 21:18:32 -0300

    adoção do docker mongo

--- nodeJs/server.js
+++ nodeJs/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/nodeapi',{useNewUrlParser:true});
+
+app.get("/",(req,re)=>{
+    res.send("Hello world");
+});
 
 app.listen(3001);
