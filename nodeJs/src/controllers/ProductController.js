764d38968b0e3c76a8499ddcc56b49a79bba50e4
Victor Almeida <victor.freitas08@hotmail.com>
2019-02-26 18:36:20 -0300

    finalizado a parte de nodejs

--- nodeJs/src/controllers/ProductController.js
+++ nodeJs/src/controllers/ProductController.js
@@ -3,7 +3,24 @@ const Product = mongoose.model("Product");
 
 module.exports={
     async index (req,res){
-        const product = await Product.find();
+        const {page = 1} = req.query;
+        const product = await Product.paginate({},{page,limit:10});
+        return res.json(product);
+    },
+    async show (req,res){
+        const product = await Product.findById(req.params.id);
+        return res.json(product);
+    },
+    async store (req,res){
+        const product = await Product.create(req.body);
+        return res.json(product);
+    },
+    async updade (req,res){
+        const product = await Product.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        return res.json(product);
+    },
+    async destroy (req,res){
+        const product = await Product.findByIdAndRemove(req.params.id);
         return res.json(product);
     }
 }
