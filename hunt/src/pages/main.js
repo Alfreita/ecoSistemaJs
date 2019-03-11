15a76b3290019f78878cd287f789c2c9fb2ca762
Victor Almeida <victor.freitas08@hotmail.com>
2019-03-11 11:44:28 -0300

    entendendo estadoe e colocando listagem de produtos em tela

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,14 +1,46 @@
 import React, {Component} from 'react';
-import {View,Text} from 'react-native';
-
+import api from '../services/api';
+import {View,Text,FlatList,TouchableOpacity} from 'react-native';
 export default class Main extends Component{
 static navigationOptions ={
     title: 'JSHunt'
 };
+
+state ={
+    docs: [],
+};
+
+componentDidMount(){
+    this.loadProducts();
+}
+
+loadProducts = async () => {
+    const response = await api.get('/products');
+
+    const { docs } = response.data;
+    this.setState({docs});
+};
+
+renderItem = ({item}) =>(
+    <View>
+        <Text>{item.title}</Text>
+        <Text>{item.description}</Text>
+        <TouchableOpacity onPress={()=>{}}>
+            <Text>Acessar</Text>
+        </TouchableOpacity>
+    </View>
+);
+
     render(){
         return (
             <View>
-                <Text>Pagina inicial</Text>
+               <FlatList 
+               data = {this.state.docs} 
+               keyExtractor={item => item._id}
+               renderItem={this.renderItem}
+               />
+
+              
             </View>
         );
     }
